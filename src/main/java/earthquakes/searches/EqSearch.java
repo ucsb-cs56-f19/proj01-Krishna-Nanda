ed5e3fa876daed471d9d0adffb722c80252c146f
Krishna Nanda <KrishnaNanda@ucsb.edu>
2019-12-04 16:55:51 -0800

    kn - Added lat and longitude parameters for EQ_S

--- src/main/java/earthquakes/searches/EqSearch.java
+++ src/main/java/earthquakes/searches/EqSearch.java
@@ -3,6 +3,9 @@ public class EqSearch {
     EqSearch(){
         distance = 0;
         minmag = 0;
+        lat = 0;
+        lon = 0;
+        location = "";
     } 
 
     public int getDistance(){
@@ -17,8 +20,27 @@ public class EqSearch {
     public void setMinmag(int m){
         minmag = m;
     }
-
+    public double getLat(){
+        return lat;
+    }
+    public double getLon(){
+        return lon;
+    }
+    public String getLocation(){
+        return location;
+    }
+    public void setLat(double d){
+        lat = d;
+    }
+    public void setLon(double d){
+        lon = d;
+    }
+    public void setLocation(String d){
+        location = d;
+    }
     private int distance;
     private int minmag;
-
+    private double lat;
+    private double lon;
+    private String location;
 }
