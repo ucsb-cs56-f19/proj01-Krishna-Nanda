49b7d924a7bf87dceb85b6cdeb6674cb84d1ebc7
Krishna Nanda <KrishnaNanda@ucsb.edu>
2019-12-05 20:00:25 -0800

    kn - Functionality for AddFav button

--- src/main/java/earthquakes/entities/Location.java
+++ src/main/java/earthquakes/entities/Location.java
@@ -16,7 +16,7 @@ public class Location {
     private double lat;
     private String name;
     private String uid;
-
+    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
